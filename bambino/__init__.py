802af23686de7459618ec3e2337bcc96818a2566
quezo <alex.quez@gmail.com>
2012-03-26 16:34:59 -0700

    Updating ignore file

--- bambino/__init__.py
+++ bambino/__init__.py
@@ -1,10 +1,10 @@
 from pyramid.config import Configurator
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """ Serve the Bambino application.
     """
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('applications', '/applications')
     config.scan()
+    
     return config.make_wsgi_app()
--- bambino/views.py
+++ bambino/views.py
@@ -1,5 +1,6 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='applications', renderer='json')
 def my_view(request):
+    # make a call to tim's code right here
     return {'project':'Bambino'}
