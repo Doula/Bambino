6d07e9067eb34668c5dc9b01916e247fb64f82fa
quezo <alex.quez@gmail.com>
2012-03-27 16:10:51 -0700

    Adding setting dor web apps directory

--- bambino/views.py
+++ bambino/views.py
@@ -4,9 +4,10 @@ import json
 
 @view_config(route_name='applications', renderer='json')
 def my_view(request):
-    # /Users/alexvazquezOLD/boxes/code_dot_corp/webappdir
-    web_app_dir = '/Users/alexvazquezOLD/boxes/code_dot_corp/webappdir'
-    appenv = AppEnv(web_app_dir)
+    web_apps_dir = request.registry.settings['web_apps_dir']
+    appenv = AppEnv(web_apps_dir)
     payload = dict(status='ok', payload=appenv.repo_data)
+    
+    print json.dumps(payload)
     
     return json.dumps(payload)
