8b4e3a2e2eb3a25a9a060be8c6e0ee5423f1d5f5
Timothy Sabat <timothy.sabat@gmail.com>
2012-09-05 13:15:58 -0700

    remove error line

--- fabfile.py
+++ fabfile.py
@@ -43,7 +43,6 @@ def create_env():
 def get_hosts():
     response = requests.get(doula_address)
     print 'RESPONSE'
-    print response.text
     print response
     response = json.loads(response.text)
     return response['ip_addresses']
